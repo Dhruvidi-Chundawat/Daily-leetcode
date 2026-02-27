4cea04770806d72fd4617a606636964784ba2ce1
Dhruvidi-Chundawat <dhruvidichundawat@gmail.com>
2026-02-27 19:21:04 +0530

    Merge sorted array

--- Leetcode_88.py
+++ Leetcode_88.py
@@ -3,7 +3,7 @@ class Solution(object):
         i = m - 1      
         j = n - 1      
         k = m + n - 1  
-
+        
         while j >= 0:
             if i >= 0 and nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
